06c3c7de6d6ddd83568e1a67a8e296bbea090a14
Ozan Tellioglu <ozntel@gmail.com>
2021-06-03 12:00:55 +0200

    Change of File Explorer Folder Detection

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
-import { Plugin } from 'obsidian';
+import { Plugin, PluginSettingTab, App, Setting } from 'obsidian';
+
+interface FileExplorerNoteCountSettings {
+	showAllNumbers: boolean,
+}
+
+const DEFAULT_SETTINGS: FileExplorerNoteCountSettings = {
+	showAllNumbers: false,
+}
 
 export default class FileExplorerNoteCount extends Plugin {
 
+	settings: FileExplorerNoteCountSettings;
+	loadedStyles: Array<HTMLStyleElement>;
+
 	async onload() {
+		this.addSettingTab(new FileExplorerNoteCountSettingsTab(this.app, this));
+		await this.loadSettings();
+		if (!this.settings.showAllNumbers) this.loadStyle();
 		this.updateFolderNumbers();
 		this.app.metadataCache.on('resolved', this.updateFolderNumbers);
-		this.registerDomEvent(document, 'click', this.updateFolderNumbers);
+		this.app.vault.on('create', this.updateFolderNumbers);
+		this.app.vault.on('rename', this.updateFolderNumbers);
+		this.app.vault.on('delete', this.updateFolderNumbers);
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
@@ -14,50 +30,61 @@ export default class FileExplorerNoteCount extends Plugin {
 		console.log('unloading plugin');
 	}
 
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
 	updateFolderNumbers = () => {
 
 		this.clearInsertedNumbers();
-
-		const allFolderTitleNodes = document.querySelectorAll('.nav-folder-title');
 
 		// Get All Available Notes under Vault
 		var mdNotes = this.app.vault.getMarkdownFiles();
 
 		// Create Folder File Map
 		const counts: { [key: string]: number } = {};
-
 		mdNotes.forEach(mdNote => {
 			for (let folder = mdNote.parent; folder != null; folder = folder.parent) {
 				counts[folder.path] = 1 + (counts[folder.path] || 0)
 			}
 		})
 
-		// Loop All Available Folder Titles
-		allFolderTitleNodes.forEach(folderTitleNode => {
+		// Loop Through File Explorer Elements
+		var fileExplorers = this.app.workspace.getLeavesOfType("file-explorer");
 
-			// Get Path of Folder Title
-			var currentDataPath = folderTitleNode.getAttr('data-path');
+		for (let fileExplorer of fileExplorers) {
+			// @ts-ignore
+			for (const [key, value] of Object.entries(fileExplorer.view.fileItems)) {
 
-			// No number for the Vault Main Folder
-			if (currentDataPath === '/') return;
+				if (value.titleEl.className === 'nav-folder-title') {
+					// Get the Title Node
+					var folderTitleNode: HTMLElement = value.titleEl;
 
-			const isCollapsed = folderTitleNode.parentElement.className.includes('is-collapsed');
+					// Get Path of Folder Title
+					var currentDataPath = folderTitleNode.getAttr('data-path');
 
-			// Check if has Child Ffolder
-			var childRegex = new RegExp(currentDataPath + '/.*/.*')
-			var hasChildFolder = mdNotes.some(mdNote => {
-				return mdNote.path.match(childRegex);
-			})
+					// No number for the Vault Main Folder
+					if (currentDataPath === '/') continue;
 
-			if (isCollapsed || (!isCollapsed && !hasChildFolder)) {
-				folderTitleNode.createDiv(
-					{
-						cls: 'oz-folder-numbers',
-						text: counts[currentDataPath].toString()
+					// Add Number of Notes
+					if (counts[currentDataPath]) {
+						var folderCount = folderTitleNode.createDiv({ cls: 'oz-folder-numbers', text: counts[currentDataPath].toString() })
+
+						// Add Attribute if There is A Children Folder for CSS
+						for (let child of value.children) {
+							if (child.children) {
+								folderCount.setAttr('haschild', 'true');
+								break;
+							}
+						}
 					}
-				)
+				}
 			}
-		})
+		}
 	}
 
 	clearInsertedNumbers = () => {
@@ -67,4 +94,67 @@ export default class FileExplorerNoteCount extends Plugin {
 		})
 	}
 
+	// Style Settings
+
+	loadStyle = () => {
+		this.loadedStyles = Array<HTMLStyleElement>(0);
+		var style = document.createElement("style");
+		style.innerHTML = collapseStyle;
+		document.head.appendChild(style);
+		this.loadedStyles.push(style);
+	}
+
+	unloadStyle = () => {
+		for (let style of this.loadedStyles) {
+			document.head.removeChild(style);
+		}
+		this.loadedStyles = Array<HTMLStyleElement>(0);
+	}
+
+	handleStyleToggle = (newStyle: boolean) => {
+		if (!newStyle) {
+			this.loadStyle();
+		} else {
+			this.unloadStyle();
+		}
+	}
+
 }
+
+class FileExplorerNoteCountSettingsTab extends PluginSettingTab {
+	plugin: FileExplorerNoteCount;
+
+	constructor(app: App, plugin: FileExplorerNoteCount) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		let { containerEl } = this;
+		containerEl.empty();
+		containerEl.createEl('h2', { text: 'File Explorer Note Count Settings' });
+
+		new Setting(containerEl)
+			.setName('Show All Numbers')
+			.setDesc('Turn on this option if you want to see the number of notes even after you expand the collapsed folders')
+			.addToggle((toggle) => toggle
+				.setValue(this.plugin.settings.showAllNumbers)
+				.onChange((value) => {
+					this.plugin.handleStyleToggle(value);
+					this.plugin.settings.showAllNumbers = value;
+					this.plugin.saveSettings();
+				})
+			)
+	}
+
+}
+
+const collapseStyle = `
+	.nav-folder:not(.is-collapsed) > .nav-folder-title > .oz-folder-numbers { 
+		display: none; 
+	}
+
+	.oz-folder-numbers:not([haschild='true']){
+		display: inline !important;
+	}
+`
